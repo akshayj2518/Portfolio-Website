a4b0ce40249940f9f31b833138d173d472978cd2
akshay <akshayj2518@gmail.com>
2022-01-31 12:39:00 +0530

    Major changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MyPortfolio';
+
+  ngOnInit() {
+    const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		(e.target as Element).classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		(e.target as Element).classList.remove('active');
+	});
+	
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
+  }
+  // console.log()
 }
